5dbb21cc0bb3cc4b94ffb0a788cd2c5e1ad904c4
ramon.rovirosa@gmail.com <ramonrovirosa@gmail.com>
2013-05-17 16:09:47 -0700

    skeleton compiles

--- project3/cachesim.c
+++ project3/cachesim.c
@@ -3,6 +3,10 @@
 #include <strings.h>
 #include "cachesim.h"
 
+void main(){
+
+}
+
 //In this function, you create the cache and initialize it, returning a pointer to the struct. Because you are determining the struct, you return a void * to our main. Type 0 is a direct-mapped cache. Type 1 is a pseudo-associative cache. Type 2 is a 4-way set associative cache. 
 Cache *createAndInitialize(int blocksize, int cachesize, int type){
    	Cache *newcache = (Cache *) malloc(sizeof(Cache));
@@ -20,23 +24,24 @@ Cache *createAndInitialize(int blocksize, int cachesize, int type){
 int accessCache(Cache *cache, int address){
 
 
-
+  return 0;
 }
 
 //This returns the number of misses that have occurred so far
 int missesSoFar(Cache *cache){
 
 
-
+   return 0;
 }
 
 //This returns the number of accesses that have occurred so far
 int accessesSoFar(Cache *cache){
 
-
+   return 0;
 }
 
 //This returns the total number of cycles that all of the accesses have taken so far. 
 int totalAccessTime(Cache *cache){
 
+   return 0;
 }

9f3fdb7ff897de4aab0998bd9c4e6ba66f77956c
ramon.rovirosa@gmail.com <ramonrovirosa@gmail.com>
2013-05-17 16:06:46 -0700

    skeletion done

--- project3/cachesim.c
+++ project3/cachesim.c
@@ -4,7 +4,7 @@
 #include "cachesim.h"
 
 //In this function, you create the cache and initialize it, returning a pointer to the struct. Because you are determining the struct, you return a void * to our main. Type 0 is a direct-mapped cache. Type 1 is a pseudo-associative cache. Type 2 is a 4-way set associative cache. 
-void *createAndInitialize(int blocksize, int cachesize, int type){
+Cache *createAndInitialize(int blocksize, int cachesize, int type){
    	Cache *newcache = (Cache *) malloc(sizeof(Cache));
 	newcache->blocksize = blocksize;
 	newcache->totalCacheSize = cachesize;
@@ -17,26 +17,26 @@ void *createAndInitialize(int blocksize, int cachesize, int type){
 }
 
 //In this function, we access the cache with a particular address. If the address results in a hit, return 1. If it is a miss, return 0.
-int accessCache(void *cache, int address){
+int accessCache(Cache *cache, int address){
 
 
 
 }
 
 //This returns the number of misses that have occurred so far
-int missesSoFar(void *cache){
+int missesSoFar(Cache *cache){
 
 
 
 }
 
 //This returns the number of accesses that have occurred so far
-int accessesSoFar(void *cache){
+int accessesSoFar(Cache *cache){
 
 
 }
 
 //This returns the total number of cycles that all of the accesses have taken so far. 
-int totalAccessTime(void *cache){
+int totalAccessTime(Cache *cache){
 
 }
--- project3/cachesim.h
+++ project3/cachesim.h
@@ -3,15 +3,21 @@
 
 //You have a struct that contains all of the information for one cache.
 typedef struct _cache{
-	int blocksize;
-	int totalCacheSize;
-	int addresses;
-	int hitTime;
-	int missPolicy;
+  int *cacheArray; //array of addresses
+  
+  int type;//type 1-direct,2-pseudo,3-fourway.
+  
+  int blocksize;   //
+  int totalCacheSize;
+
+  int missesSoFar;
+  int accessesSoFar;
+  int totalAccessTime;
+
 } Cache;
 
 
-void *createAndInitialize(int blocksize, int cachesize, int type);
+Cache *createAndInitialize(int blocksize, int cachesize, int type);
 //In this function, you create the cache and initialize it, returning a pointer to the struct. Because you are determining the struct, you return a void * to our main. Type 0 is a direct-mapped cache. Type 1 is a pseudo-associative cache. Type 2 is a 4-way set associative cache. 
 
 int accessCache(void *cache, int address);

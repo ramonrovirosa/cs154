e02156217ed2d5b860e4808428f263ec1eab7acb
AkiStankoski <astankoski@gmail.com>
2013-05-30 16:16:47 -0700

    shift pc right by 2 to fix issues

--- project4-Aki/branchsim.c
+++ project4-Aki/branchsim.c
@@ -14,14 +14,14 @@ BP *createAndInitialize(int numEntries){
 }
 int accessBranchPredictor(BP *bp, int PC){
 //Given the PC of a branch, report whether this branch is predicted as taken or not taken. (1 = taken, 0 = not taken)
-	int index = (PC & (bp->tablesize - 1));
+	int index = ((PC>>2) & (bp->tablesize - 1));
 	int state = bp->ptable[index];
 	bp->accesses++;
 	return (state == 2 || state == 3) ? 1 : 0; //if state 2 or 3 return 1 else return 0
 }
 void updateBranchPredictor(BP *bp, int PC, int result){
 //Given the PC of the branch and the actual result (1 = taken, 0 = not taken), update the branch predictor for that entry in the table.
-	int index = (PC & (bp->tablesize - 1));
+	int index = ((PC>>2) & (bp->tablesize - 1));
 	int prediction = bp->ptable[index];
 	
 	switch(prediction){
--- project4-Aki/bsimGolf.c
+++ project4-Aki/bsimGolf.c
@@ -5,15 +5,14 @@
 
 BP *createAndInitialize(int numEntries){ //Allocate a table with numEntries entries, each initialized to the not-taken state. numEntries will be a power of 2.
 	BP *newBP = malloc(sizeof(BP));
-	*newBP = (BP){.tablesize=0, .accesses=0, .mispredictions=0};
+	*newBP = (BP){.tablesize=numEntries, .accesses=0, .mispredictions=0};
 	newBP->ptable = calloc(numEntries,sizeof(int)); // avoids for loop to initialize to 0s
 	return newBP;
 }
 int accessBranchPredictor(BP *bp, int PC){ //Given the PC of a branch, report whether this branch is predicted as taken or not taken. (1 = taken, 0 = not taken)
 	int index = (PC & (bp->tablesize - 1));
-	int state = bp->ptable[index];
 	bp->accesses++;
-	return (state == 2 || state == 3) ? 1 : 0; //if state 2 or 3 return 1 else return 0
+	return (bp->ptable[index] == 2 || bp->ptable[index] == 3) ? 1 : 0; //if state 2 or 3 return 1 else return 0
 }
 void updateBranchPredictor(BP *bp, int PC, int result){ //Given the PC of the branch and the actual result (1 = taken, 0 = not taken), update the branch predictor for that entry in the table.
 	int index = (PC & (bp->tablesize - 1));
